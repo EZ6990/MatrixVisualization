3e32596d9bcb0c62898711096b404e2cac08de25
tal_malul <tal.malul4@gmail.com>
2020-05-12 00:47:01 +0300

    added popups

--- Controller/CView.py
+++ Controller/CView.py
@@ -3,14 +3,16 @@ from kivy.lang import Builder
 from kivy.properties import ObjectProperty
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
+from kivy.uix.popup import Popup
 
 from Controller.MatrixView import MatrixView
 from DataObject import CMatrix
 
+
+
 Builder.load_file('View/cview.kv')
 
 class CView(BoxLayout):
-
 
     cmatrix = ObjectProperty(None)
 
@@ -18,6 +20,7 @@ class CView(BoxLayout):
         super(CView, self).__init__()
         self.bind(cmatrix=self.refresh)
         self.cmatrix = kwargs.get('cmatrix', None)
+        self.allow_popup = kwargs.get('allow_popup', True)
 
     def refresh(self, instance, value):
         if self.cmatrix is not None and self.cmatrix.size > 0:
@@ -28,6 +31,10 @@ class CView(BoxLayout):
                 self.add_widget(Label(text="+", size_hint=(0.2,1)))
                 self.add_widget(MatrixView(matrix=(self.cmatrix.matrices[i] * self.cmatrix.pattern[i])))
 
+    def zoom(self, touch):
+        if self.allow_popup and touch.button == 'left' and self.collide_point(touch.x, touch.y):
+            popup = Popup(title='C Matrix View',content=CView(cmatrix=self.cmatrix, allow_popup=False),size_hint=(1,0.3))
+            popup.open()
 
 
 
--- Controller/MatrixView.py
+++ Controller/MatrixView.py
@@ -1,6 +1,7 @@
 from kivy.lang import Builder
 from kivy.uix.gridlayout import GridLayout
 from Controller.MatrixCell import MatrixCell
+from kivy.uix.popup import Popup
 from DataObject import Matrix
 
 Builder.load_file('View/matrixview.kv')
@@ -9,6 +10,7 @@ class MatrixView(GridLayout):
     def __init__(self, **kwargs):
         super(MatrixView, self).__init__()
         self.matrix = kwargs.get('matrix', None)
+        self.allow_popup = kwargs.get('allow_popup', True)
         self.cols = self.matrix.size[0]
         self.initialize()
 
@@ -18,6 +20,10 @@ class MatrixView(GridLayout):
                 self.add_widget(MatrixCell(value=self.matrix.matrix[i][j]))
 
 
+    def zoom(self, touch):
+        if self.allow_popup and touch.button == 'left' and self.collide_point(touch.x, touch.y):
+            popup = Popup(title='Matrix View',content=MatrixView(matrix=self.matrix,allow_popup=False))
+            popup.open()
 
 
 
